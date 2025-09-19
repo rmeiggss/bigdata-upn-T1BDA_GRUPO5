706cbc135239a0f663ae7d2cfd02fb8c9e0aa982
Roberto Meiggs <soportedesarrollos@ayesa.com>
2025-09-18 23:05:06 -0500

    Agregar archivos de proyecto.

---
+++ T1BDA_GRUPO5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace T1BDA_GRUPO5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
